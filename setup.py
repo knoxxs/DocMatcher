ff329919e4c83687fd1d4890fbb7f392a35f524a
Abhishek Gupta <abhi.bansal21@gmail.com>
2014-01-02 15:55:39 +0530

    Write basic setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+ 
+setup(
+    name='DocMatcher',
+    version='0.1.0',
+    author='Abhishek Gupta',
+    author_email='abhi.bansal21+DocMatcher_module.com',
+    # packages=['towelstuff', 'towelstuff.test'],
+    # scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    # url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE.txt',
+    description='A matcher to match original document with the errorneous one',
+    long_description=open('README.txt').read(),
+    # install_requires=[
+    #     "Django >= 1.1.1",
+    #     "caldav == 0.1.4",
+    # ],
+)
